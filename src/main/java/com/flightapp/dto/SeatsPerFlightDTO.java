65f0ad90d0e35110620085e601e7feb874caa8de
JoshikaSree <Joshikasreepolaku@gmail.com>
2025-11-23 18:58:35 +0530

    added lines of code

--- src/main/java/com/flightapp/dto/SeatsPerFlightDTO.java
+++ src/main/java/com/flightapp/dto/SeatsPerFlightDTO.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Setter
 public class SeatsPerFlightDTO {
 
+    // Used to show how many seats were booked for each flight.
     private String flightId;
     private long totalSeatsBooked;
 }
