394757cbe14f47ebdf42a4cec4ba64365b81fff3
JoshikaSree <Joshikasreepolaku@gmail.com>
2025-11-23 19:05:43 +0530

    addeed lines of code

--- src/main/java/com/flightapp/repository/SeatRepository.java
+++ src/main/java/com/flightapp/repository/SeatRepository.java
@@ -6,7 +6,9 @@ import reactor.core.publisher.Flux;
 
 public interface SeatRepository extends ReactiveMongoRepository<Seat, String> {
 
+    // Returns all seats belonging to a flight.
     Flux<Seat> findByFlightId(String flightId);
 
+    // Returns seats filtered by their status .
     Flux<Seat> findByFlightIdAndStatus(String flightId, String status);
 }
