b7e6c5077c4d6927155ddfdc4780d70f31399d72
JoshikaSree <Joshikasreepolaku@gmail.com>
2025-11-23 19:28:51 +0530

    Fix: removed unused Mono import

--- src/main/java/com/flightapp/repository/FlightRepository.java
+++ src/main/java/com/flightapp/repository/FlightRepository.java
@@ -5,7 +5,6 @@ import com.flightapp.model.Flight;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 
