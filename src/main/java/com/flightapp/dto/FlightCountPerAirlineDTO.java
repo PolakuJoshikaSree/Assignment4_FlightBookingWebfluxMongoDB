463a2a4fd1b989597bf34c5796268c5bcd3d972f
JoshikaSree <Joshikasreepolaku@gmail.com>
2025-11-23 18:55:30 +0530

    added code lines

--- src/main/java/com/flightapp/dto/FlightCountPerAirlineDTO.java
+++ src/main/java/com/flightapp/dto/FlightCountPerAirlineDTO.java
@@ -6,6 +6,8 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class FlightCountPerAirlineDTO {
+
+    // Used to return how many flights each airline has.
     private String airlineCode;
     private String airlineName;
     private int totalFlights;
